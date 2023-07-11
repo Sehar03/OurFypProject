4af5d1f3e44fcefbae5607d89f679c35ca32f42e
Ayesha2542 <123588676+Ayesha2542@users.noreply.github.com>
2023-07-11 10:13:17 -0700

    hy
    i change something in code

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView ,Image} from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -17,7 +19,7 @@ const HomeScreen=()=>{
 const DetailsScreen=()=> {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+      <Text>Detail Screen</Text>
     </View>
   );
 }
@@ -37,3 +39,16 @@ const App=() =>{
 }
 
 export default App;
+
+
+// const App=()=>{
+//   return(
+// <SafeAreaView>
+// <View style={{backgroundColor:"darkred",height:800,width:400}}>
+//   <Image source={{uri: 'https://static.vecteezy.com/system/resources/previews/007/679/655/original/people-eating-talking-drinking-and-working-at-tables-on-cafe-or-restaurant-in-flat-cartoon-illustration-vector.jpg'}} style={{  height:hp('47'),width:wp('99'),marginTop:100,marginLeft:10}} />
+//   <Text style={{fontSize:25,textAlign:"center",fontWeight:"bold",color:"white"}}>Food For Each</Text>
+//   </View>
+// </SafeAreaView>
+//   )
+// }
+// export default App;
